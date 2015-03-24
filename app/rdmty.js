674bf71d23af10faeeeeb34e401f7b33fdeb169a
Billy Vong <billyvg@gmail.com>
2015-03-24 13:45:50 -0700

    Change from JS to JSX, also refactored to support different charts (dmg, healing for now)

--- app/rdmty.js
+++ app/rdmty.js
@@ -1,113 +1,129 @@
 // fiddle: http://jsfiddle.net/v1ddnsvh/8/
+/* global window */
 
 var IMAGE_PATH = 'images';
 
-var CombatantCompact = React.createClass({
-	getDefaultProps: function() {
-		return {
-			onClick: function() {}
-		};
-	},
+var React = window.React;
 
-	jobImage: function(job) {
+var ____Class2P=React.Component;for(var ____Class2P____Key in ____Class2P){if(____Class2P.hasOwnProperty(____Class2P____Key)){CombatantCompact[____Class2P____Key]=____Class2P[____Class2P____Key];}}var ____SuperProtoOf____Class2P=____Class2P===null?null:____Class2P.prototype;CombatantCompact.prototype=Object.create(____SuperProtoOf____Class2P);CombatantCompact.prototype.constructor=CombatantCompact;CombatantCompact.__superConstructor__=____Class2P;function CombatantCompact(){"use strict";if(____Class2P!==null){____Class2P.apply(this,arguments);}}
+    Object.defineProperty(CombatantCompact.prototype,"jobImage",{writable:true,configurable:true,value:function(job) {"use strict";
         if (window.JSFIDDLE) {
             return window.GLOW_ICONS[job.toLowerCase()];
         }
 
-		return IMAGE_PATH + '/jobs/' + job + '_glow.png';
-	},
+        return IMAGE_PATH + '/jobs/' + job + '_glow.png';
+    }});
 
-	render: function() {
-		//var width = parseInt(this.props.data.damage / this.props.encounterDamage * 100, 10) + '%';
-		var width = parseInt(this.props.data.damage / this.props.maxdps * 100, 10) + '%';
+    Object.defineProperty(CombatantCompact.prototype,"render",{writable:true,configurable:true,value:function() {"use strict";
+        //var width = parseInt(this.props.data.damage / this.props.encounterDamage * 100, 10) + '%';
+        var width = parseInt(this.props.total / this.props.max * 100, 10) + '%';
 
-		return (
-			this.props.data.dps === '---' ? null :
-			React.createElement('li', {
-					className: 'row ' + this.props.data.Job.toLowerCase() + (this.props.isSelf ? ' self' : ''),
-					onClick: this.props.onClick
-				},
-				React.createElement('div', {className: 'bar', style: {width: width}}),
-				React.createElement('div', {className: 'text-overlay'},
-					React.createElement('span', {className: 'job-icon'},
-						React.createElement('img', {src: this.jobImage(this.props.data.Job.toLowerCase())})
-					),
-					React.createElement('span', {className: 'rank'}, this.props.rank + '. '),
-					React.createElement('span', {className: 'character-name'},
-						this.props.data.name
-					),
-					React.createElement('span', {className: 'character-job'},
-						this.props.data.Job
-					),
-					React.createElement('div', {className: 'damage-stats'},
-						React.createElement('span', {className: 'damage'},
-							this.props.data.damage
-						),
-						' (',
-						React.createElement('span', {className: 'dps'},
-							this.props.data.dps
-						),
-						', ',
-						React.createElement('span', {className: 'damage-percent'},
-							this.props.data['damage%']
-						),
-						')'
-					)
-				)
-			)
-		);
-	}
-});
+        return (
+            this.props.perSecond === '---' ? null :
+            React.createElement("li", {
+                className: 'row ' + this.props.job.toLowerCase() + (this.props.isSelf ? ' self' : ''), 
+                onClick: this.props.onClick}, 
+                React.createElement("div", {
+                    className: "bar", 
+                    style: {width: width}}), 
+                    React.createElement("div", {className: "text-overlay"}, 
+                        React.createElement("span", {className: "job-icon"}, 
+                            React.createElement("img", {src: this.jobImage(this.props.job.toLowerCase())})
+                        ), 
+                        React.createElement("span", {className: "rank"}, 
+                            (this.props.rank + ". ")
+                        ), 
+                        React.createElement("span", {className: "character-name"}, 
+                            this.props.characterName
+                        ), 
+                        React.createElement("span", {className: "character-job"}, 
+                            this.props.job
+                        ), 
+                        React.createElement("div", {className: "damage-stats"}, 
+                            React.createElement("span", {className: "damage"}, 
+                                this.props.total
+                            ), 
+                            "(", 
+                            React.createElement("span", {className: "dps"}, 
+                                this.props.perSecond, ","
+                            ), 
 
-var Encounter = React.createClass({
-    shouldComponentUpdate: function(nextProps) {
-    },
+                            React.createElement("span", {className: "damage-percent"}, 
+                                this.props.percentage
+                            ), 
+                            ")"
+                        )
+                    )
+            )
+        );
+    }});
 
-    render: function() {
-        var dps = this.props.encdps.length <= 7 ? this.props.encdps : this.props.ENCDPS;
-        var rdps = parseFloat(this.props.encdps);
+CombatantCompact.defaultProps = {
+    onClick:function() {}
+};
+
+var ____Class2Q=React.Component;for(var ____Class2Q____Key in ____Class2Q){if(____Class2Q.hasOwnProperty(____Class2Q____Key)){ChartView[____Class2Q____Key]=____Class2Q[____Class2Q____Key];}}var ____SuperProtoOf____Class2Q=____Class2Q===null?null:____Class2Q.prototype;ChartView.prototype=Object.create(____SuperProtoOf____Class2Q);ChartView.prototype.constructor=ChartView;ChartView.__superConstructor__=____Class2Q;function ChartView(){"use strict";if(____Class2Q!==null){____Class2Q.apply(this,arguments);}}
+    Object.defineProperty(ChartView.prototype,"render",{writable:true,configurable:true,value:function() {"use strict";
+        return (
+            React.createElement("div", {className: "chart-view"}
+            )
+        );
+    }});
+
+
+var ____Class2R=React.Component;for(var ____Class2R____Key in ____Class2R){if(____Class2R.hasOwnProperty(____Class2R____Key)){Header[____Class2R____Key]=____Class2R[____Class2R____Key];}}var ____SuperProtoOf____Class2R=____Class2R===null?null:____Class2R.prototype;Header.prototype=Object.create(____SuperProtoOf____Class2R);Header.prototype.constructor=Header;Header.__superConstructor__=____Class2R;function Header(){"use strict";if(____Class2R!==null){____Class2R.apply(this,arguments);}}
+    Object.defineProperty(Header.prototype,"render",{writable:true,configurable:true,value:function() {"use strict";
+        let encounter = this.props.encounter;
+        var dps = encounter.encdps.length <= 7 ? encounter.encdps : encounter.ENCDPS;
+        var rdps = parseFloat(encounter.encdps);
         var rdps_max = 0;
 
-        if (!isNaN(rdps) && rdps != Infinity) {
+        if (!isNaN(rdps) && rdps !== Infinity) {
             rdps_max = Math.max(rdps_max, rdps);
         }
 
         var width = (rdps / rdps_max) * 100;
 
         return (
-            React.createElement('div', {className: 'encounter-header'},
-                React.createElement('span', {className: 'target-name'},
-                    this.props.title
-                ),
-                React.createElement('span', {className: 'duration'},
-                    this.props.duration
+            React.createElement("div", {className: "encounter-header"}, 
+                React.createElement("span", {className: "target-name"}, 
+                    encounter.title
+                ), 
+                React.createElement("span", {className: "duration"}, 
+                    "(", encounter.duration, ")"
+                ), 
+
+                React.createElement("div", {
+                    className: "chart-view-switcher", 
+                    onClick: this.props.onViewChange}, 
+                    this.props.currentView
                 )
             )
         );
-    }
-});
+    }});
+;
+
+Header.defaultProps = {
+    encounter: {},
+    onViewChange:function() {}
+}
 
 
-var Combatants = React.createClass({
-    getDefaultProps: function() {
-        return {
-            onClick: function() {}
-        };
-    },
-
-    shouldComponentUpdate: function(nextProps) {
+var ____Class2S=React.Component;for(var ____Class2S____Key in ____Class2S){if(____Class2S.hasOwnProperty(____Class2S____Key)){Combatants[____Class2S____Key]=____Class2S[____Class2S____Key];}}var ____SuperProtoOf____Class2S=____Class2S===null?null:____Class2S.prototype;Combatants.prototype=Object.create(____SuperProtoOf____Class2S);Combatants.prototype.constructor=Combatants;Combatants.__superConstructor__=____Class2S;function Combatants(){"use strict";if(____Class2S!==null){____Class2S.apply(this,arguments);}}
+    Object.defineProperty(Combatants.prototype,"shouldComponentUpdate",{writable:true,configurable:true,value:function(nextProps) {"use strict";
         // if data is empty then don't re-render
         if (Object.getOwnPropertyNames(nextProps.data).length === 0) {
             return false;
         }
 
         return true;
-    },
+    }});
 
-    render: function() {
+    Object.defineProperty(Combatants.prototype,"render",{writable:true,configurable:true,value:function() {"use strict";
         var rows = [];
         var maxRows = 12;
-        var dataArray = Object.keys(this.props.data);
+        var isDataArray = _.isArray(this.props.data);
+        var dataArray = isDataArray ? this.props.data : Object.keys(this.props.data);
         var limit = Math.max(dataArray.length, maxRows);
         var names = dataArray.slice(0, maxRows-1);
         var maxdps = false;
@@ -115,66 +131,144 @@ var Combatants = React.createClass({
         var row;
         var isSelf;
         var rank = 1;
+        var stats;
 
         for (var i = 0; i < names.length; i++) {
-            combatant = this.props.data[names[i]];
+            combatant = isDataArray ? this.props.data[i] : this.props.data[names[i]];
+            stats = null;
 
-            if (!maxdps) {
-                maxdps = parseFloat(combatant.damage);
-            }
-
+            console.log(combatant);
             isSelf = combatant.name === 'YOU' || combatant.name === 'You';
 
             if (combatant.Job !== "") {
-                rows.push(
-                    React.createElement(CombatantCompact, {
-                    onClick: this.props.onClick,
-                    encounterDamage: this.props.encounterDamage,
-                    rank: rank,
-                    data: combatant,
-                    isSelf: isSelf,
-                    key: names[i],
-                    maxdps: maxdps
-                })
-                );
-                rank++;
+                if (this.props.currentView === 'Healing') {
+                    if (parseInt(combatant.healed, 10) > 0) {
+                        if (!maxdps) {
+                            maxdps = parseFloat(combatant.healed);
+                        }
+                        stats = {
+                            job: combatant.Job || '',
+                            characterName: combatant.name,
+                            total: combatant.healed,
+                            perSecond: combatant.enchps,
+                            percentage: combatant['healed%']
+                        }
+                    }
+                }
+                else {
+                    if (!maxdps) {
+                        maxdps = parseFloat(combatant.damage);
+                    }
+                    stats = {
+                        job: combatant.Job || '',
+                        characterName: combatant.name,
+                        total: combatant.damage,
+                        perSecond: combatant.dps,
+                        percentage: combatant['damage%']
+                    }
+                }
+
+                if (stats) {
+                    rows.push(
+                        React.createElement(CombatantCompact, React.__spread({
+                            onClick: this.props.onClick, 
+                            encounterDamage: this.props.encounterDamage, 
+                            rank: rank, 
+                            data: combatant, 
+                            isSelf: isSelf, 
+                            key: combatant.name, 
+                            max: maxdps}, 
+                            stats)
+                        )
+                    );
+                    rank++;
+                }
             }
 
         }
 
-        return React.createElement('ul', {className: 'combatants'}, rows);
-    }
-});
-
-var DamageMeter = React.createClass({
-    getDefaultProps: function() {
-        return {
-            parseData: {},
-            noJobColors: false
-        };
-    },
-
-    handleCombatRowClick: function(e) {
-        console.log('Row clicked');
-    },
-
-    handleClick: function(e) {
-        console.log('clicked');
-    },
-
-    render: function() {
         return (
-            React.createElement('div', {
-                onClick: this.handleClick,
-                className: 'damage-meter' + (!this.props.parseData.isActive ? ' inactive' : '') + (!this.props.noJobColors ? ' show-job-colors' : '')
-            },
-                React.createElement(Encounter, this.props.parseData.Encounter),
-                React.createElement(Combatants, {
-                    onClick: this.handleCombatRowClick,
-                    data: this.props.parseData.Combatant,
-                    encounterDamage: this.props.parseData.Encounter.damage
-                })
+            React.createElement("ul", {className: "combatants"}, 
+                rows
             )
         );
+    }});
+
+
+Combatants.defaultProps = {
+    onClick:function() {}
+};
+
+var ____Class2T=React.Component;for(var ____Class2T____Key in ____Class2T){if(____Class2T.hasOwnProperty(____Class2T____Key)){DamageMeter[____Class2T____Key]=____Class2T[____Class2T____Key];}}var ____SuperProtoOf____Class2T=____Class2T===null?null:____Class2T.prototype;DamageMeter.prototype=Object.create(____SuperProtoOf____Class2T);DamageMeter.prototype.constructor=DamageMeter;DamageMeter.__superConstructor__=____Class2T;
+    function DamageMeter(props) {"use strict";
+        ____Class2T.call(this,props);
+        this.state = {
+            currentViewIndex: 0
+        };
     }
-});
+
+    Object.defineProperty(DamageMeter.prototype,"handleCombatRowClick",{writable:true,configurable:true,value:function(e) {"use strict";
+        console.log('Row clicked');
+    }});
+
+    Object.defineProperty(DamageMeter.prototype,"handleClick",{writable:true,configurable:true,value:function(e) {"use strict";
+    }});
+
+    Object.defineProperty(DamageMeter.prototype,"handleViewChange",{writable:true,configurable:true,value:function(e) {"use strict";
+        var index = this.state.currentViewIndex;
+
+        if (index > this.props.chartViews.length-2) {
+            index = 0;
+        }
+        else {
+            index++;
+        }
+
+        this.setState({
+            currentViewIndex: index
+        });
+
+    }});
+
+    Object.defineProperty(DamageMeter.prototype,"render",{writable:true,configurable:true,value:function() {"use strict";
+        var data = this.props.parseData.Combatant;
+
+        // need to resort data if currentView is not damage
+        if (this.state.currentViewIndex === 1) {
+            data = _.sortBy(_.filter(data, function(d)  {
+                return parseInt(d.healed, 10) > 0;
+            }), function(d)  {
+                if (this.state.currentViewIndex === 1) {
+                    return -parseInt(d.healed, 10);
+                }
+            }.bind(this));
+        }
+
+        return (
+            React.createElement("div", {
+                onClick: this.handleClick, 
+                className: 'damage-meter' + (!this.props.parseData.isActive ? ' inactive' : '') + (!this.props.noJobColors ? ' show-job-colors' : '')}, 
+                React.createElement(Header, {
+                    encounter: this.props.parseData.Encounter, 
+                    onViewChange: this.handleViewChange.bind(this), 
+                    currentView: this.props.chartViews[this.state.currentViewIndex]}
+                    ), 
+                React.createElement(Combatants, {
+                    currentView: this.props.chartViews[this.state.currentViewIndex], 
+                    onClick: this.handleCombatRowClick, 
+                    data: data, 
+                    encounterDamage: this.props.parseData.Encounter.damage})
+            )
+        );
+    }});
+
+
+DamageMeter.defaultProps = {
+    chartViews: [
+        'Damage',
+        'Healing',
+        'Detailed'
+    ],
+    parseData: {},
+    noJobColors: false
+};
---
+++ app/rdmty.jsx
@@ -0,0 +1,274 @@
+// fiddle: http://jsfiddle.net/v1ddnsvh/8/
+/* global window */
+
+var IMAGE_PATH = 'images';
+
+var React = window.React;
+
+class CombatantCompact extends React.Component {
+    jobImage(job) {
+        if (window.JSFIDDLE) {
+            return window.GLOW_ICONS[job.toLowerCase()];
+        }
+
+        return IMAGE_PATH + '/jobs/' + job + '_glow.png';
+    }
+
+    render() {
+        //var width = parseInt(this.props.data.damage / this.props.encounterDamage * 100, 10) + '%';
+        var width = parseInt(this.props.total / this.props.max * 100, 10) + '%';
+
+        return (
+            this.props.perSecond === '---' ? null :
+            <li
+                className={'row ' + this.props.job.toLowerCase() + (this.props.isSelf ? ' self' : '')}
+                onClick={this.props.onClick}>
+                <div
+                    className='bar'
+                    style={{width: width}} />
+                    <div className="text-overlay">
+                        <span className='job-icon'>
+                            <img src={this.jobImage(this.props.job.toLowerCase())} />
+                        </span>
+                        <span className="rank">
+                            {`${this.props.rank}. `}
+                        </span>
+                        <span className="character-name">
+                            {this.props.characterName}
+                        </span>
+                        <span className="character-job">
+                            {this.props.job}
+                        </span>
+                        <div className="damage-stats">
+                            <span className="damage">
+                                {this.props.total}
+                            </span>
+                            (
+                            <span className="dps">
+                                {this.props.perSecond},
+                            </span>
+
+                            <span className="damage-percent">
+                                {this.props.percentage}
+                            </span>
+                            )
+                        </div>
+                    </div>
+            </li>
+        );
+    }
+}
+CombatantCompact.defaultProps = {
+    onClick() {}
+};
+
+class ChartView extends React.Component {
+    render() {
+        return (
+            <div className="chart-view">
+            </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        let encounter = this.props.encounter;
+        var dps = encounter.encdps.length <= 7 ? encounter.encdps : encounter.ENCDPS;
+        var rdps = parseFloat(encounter.encdps);
+        var rdps_max = 0;
+
+        if (!isNaN(rdps) && rdps !== Infinity) {
+            rdps_max = Math.max(rdps_max, rdps);
+        }
+
+        var width = (rdps / rdps_max) * 100;
+
+        return (
+            <div className="encounter-header">
+                <span className="target-name">
+                    {encounter.title}
+                </span>
+                <span className="duration">
+                    ({encounter.duration})
+                </span>
+
+                <div
+                    className="chart-view-switcher"
+                    onClick={this.props.onViewChange}>
+                    {this.props.currentView}
+                </div>
+            </div>
+        );
+    }
+};
+
+Header.defaultProps = {
+    encounter: {},
+    onViewChange() {}
+}
+
+
+class Combatants extends React.Component {
+    shouldComponentUpdate(nextProps) {
+        // if data is empty then don't re-render
+        if (Object.getOwnPropertyNames(nextProps.data).length === 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    render() {
+        var rows = [];
+        var maxRows = 12;
+        var isDataArray = _.isArray(this.props.data);
+        var dataArray = isDataArray ? this.props.data : Object.keys(this.props.data);
+        var limit = Math.max(dataArray.length, maxRows);
+        var names = dataArray.slice(0, maxRows-1);
+        var maxdps = false;
+        var combatant;
+        var row;
+        var isSelf;
+        var rank = 1;
+        var stats;
+
+        for (var i = 0; i < names.length; i++) {
+            combatant = isDataArray ? this.props.data[i] : this.props.data[names[i]];
+            stats = null;
+
+            console.log(combatant);
+            isSelf = combatant.name === 'YOU' || combatant.name === 'You';
+
+            if (combatant.Job !== "") {
+                if (this.props.currentView === 'Healing') {
+                    if (parseInt(combatant.healed, 10) > 0) {
+                        if (!maxdps) {
+                            maxdps = parseFloat(combatant.healed);
+                        }
+                        stats = {
+                            job: combatant.Job || '',
+                            characterName: combatant.name,
+                            total: combatant.healed,
+                            perSecond: combatant.enchps,
+                            percentage: combatant['healed%']
+                        }
+                    }
+                }
+                else {
+                    if (!maxdps) {
+                        maxdps = parseFloat(combatant.damage);
+                    }
+                    stats = {
+                        job: combatant.Job || '',
+                        characterName: combatant.name,
+                        total: combatant.damage,
+                        perSecond: combatant.dps,
+                        percentage: combatant['damage%']
+                    }
+                }
+
+                if (stats) {
+                    rows.push(
+                        <CombatantCompact
+                            onClick={this.props.onClick}
+                            encounterDamage={this.props.encounterDamage}
+                            rank={rank}
+                            data={combatant}
+                            isSelf={isSelf}
+                            key={combatant.name}
+                            max={maxdps}
+                            {...stats}
+                        />
+                    );
+                    rank++;
+                }
+            }
+
+        }
+
+        return (
+            <ul className="combatants">
+                {rows}
+            </ul>
+        );
+    }
+}
+
+Combatants.defaultProps = {
+    onClick() {}
+};
+
+class DamageMeter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentViewIndex: 0
+        };
+    }
+
+    handleCombatRowClick(e) {
+        console.log('Row clicked');
+    }
+
+    handleClick(e) {
+    }
+
+    handleViewChange(e) {
+        var index = this.state.currentViewIndex;
+
+        if (index > this.props.chartViews.length-2) {
+            index = 0;
+        }
+        else {
+            index++;
+        }
+
+        this.setState({
+            currentViewIndex: index
+        });
+
+    }
+
+    render() {
+        var data = this.props.parseData.Combatant;
+
+        // need to resort data if currentView is not damage
+        if (this.state.currentViewIndex === 1) {
+            data = _.sortBy(_.filter(data, (d) => {
+                return parseInt(d.healed, 10) > 0;
+            }), (d) => {
+                if (this.state.currentViewIndex === 1) {
+                    return -parseInt(d.healed, 10);
+                }
+            });
+        }
+
+        return (
+            <div
+                onClick={this.handleClick}
+                className={'damage-meter' + (!this.props.parseData.isActive ? ' inactive' : '') + (!this.props.noJobColors ? ' show-job-colors' : '')}>
+                <Header
+                    encounter={this.props.parseData.Encounter}
+                    onViewChange={this.handleViewChange.bind(this)}
+                    currentView={this.props.chartViews[this.state.currentViewIndex]}
+                    />
+                <Combatants
+                    currentView={this.props.chartViews[this.state.currentViewIndex]}
+                    onClick={this.handleCombatRowClick}
+                    data={data}
+                    encounterDamage={this.props.parseData.Encounter.damage} />
+            </div>
+        );
+    }
+}
+
+DamageMeter.defaultProps = {
+    chartViews: [
+        'Damage',
+        'Healing',
+        'Detailed'
+    ],
+    parseData: {},
+    noJobColors: false
+};
